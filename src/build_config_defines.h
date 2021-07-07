ec8773ce532d3f37d5109f2f484a8eb05910c372
Степан <stepadunaevskij@gmail.com>
2021-07-07 22:35:20 +0400

    Add: Added HQ Geometry fix support
    But you can compile render without it. Because it requires shaders with this fix support

---
+++ src/build_config_defines.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// Build config defines
+
+
+// Render
+
+// HQ Geometry fix by Shoker.
+// It will work correctly only with skin.h 
+// shader with HQ geometry fix support.
+// #define HQ_GEOMETRY_FIX
+
